4808ff247ae3b9264a28b482603770f9ef498658
vidyar <vidyar@qhode.com>
2013-10-29 15:35:45 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 import os, redis
 
-redis_port = os.environ.get("SHIPPABLE_REDIS_PORT")
+redis_port = 6379
 redis_server = redis.Redis("localhost", port = int(redis_port))
 redis_server.set("name", "Shippable Redis Demo")
 print redis_server.get("name")
